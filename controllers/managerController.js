f177ba56a2f0a8aa1dc4cd8929c3edacb1aa78f0
fatormd <agenciafgm@gmail.com>
2025-10-20 13:34:10 -0300

    Update managerController.js

--- controllers/managerController.js
+++ controllers/managerController.js
@@ -1,6 +1,7 @@
-// --- CONTROLLERS/MANAGERCONTROLLER.JS ---
+// --- CONTROLLERS/MANAGERCONTROLLER.JS (Painel 4) ---
+
 export const openManagerAuthModal = (action, payload) => { 
     alert(`Gerencial: Autenticação de senha para a ação ${action} (Em desenvolvimento).`); 
 };
-export const handleTransferenciaSeletiva = () => { console.log('ManagerController: Lógica de Transferência...'); };
-export const handleExclusaoMassa = () => { console.log('ManagerController: Lógica de Exclusão...'); };
+export const handleTransferenciaSeletiva = () => { console.log('ManagerController: Lógica de Transferência (Placeholder).'); };
+export const handleExclusaoMassa = () => { console.log('ManagerController: Lógica de Exclusão (Placeholder).'); };
