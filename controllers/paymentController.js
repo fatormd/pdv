9eca6c3d5ec746f7ba6472fa958297546bc4532c
fatormd <agenciafgm@gmail.com>
2025-10-20 13:33:11 -0300

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,4 +1,5 @@
-// --- CONTROLLERS/PAYMENTCONTROLLER.JS ---
-export const renderPaymentSummary = () => { console.log('PaymentController: Renderizando Resumo de Pagamento...'); };
-export const renderPaymentSplits = () => { console.log('PaymentController: Renderizando Divisão de Contas...'); };
+// --- CONTROLLERS/PAYMENTCONTROLLER.JS (Painel 3) ---
+
+export const renderPaymentSummary = () => { console.log('PaymentController: Renderizando Resumo de Pagamento (Placeholder).'); };
+export const renderPaymentSplits = () => { console.log('PaymentController: Renderizando Divisão de Contas (Placeholder).'); };
 export const handleFinalizeOrder = () => { alert('Função de Fechamento de Conta em desenvolvimento.'); };
