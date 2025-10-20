eedc737e7d8e5739412970598d0a3e382ad38351
fatormd <agenciafgm@gmail.com>
2025-10-20 13:27:51 -0300

    Create paymentController.js

---
+++ controllers/paymentController.js
@@ -0,0 +1,4 @@
+// --- CONTROLLERS/PAYMENTCONTROLLER.JS ---
+export const renderPaymentSummary = () => { console.log('PaymentController: Renderizando Resumo de Pagamento...'); };
+export const renderPaymentSplits = () => { console.log('PaymentController: Renderizando Divisão de Contas...'); };
+export const handleFinalizeOrder = () => { alert('Função de Fechamento de Conta em desenvolvimento.'); };
