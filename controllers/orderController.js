750e2982a3b48e739a8fa307beaaf4a0ca1f1cdb
fatormd <agenciafgm@gmail.com>
2025-10-20 16:24:20 -0300

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,6 @@
 // --- CONTROLLERS/ORDERCONTROLLER.JS (Painel 2) ---
 
 // Implementação mínima de renderização do Menu (Será melhorada)
-export const renderMenu = () => { console.log('OrderController: Renderizando Menu (Placeholder).'); };
-export const renderOrderScreen = () => { console.log('OrderController: Renderizando Itens Selecionados (Placeholder).'); };
+export const renderMenu = () => { console.log('OrderController: Renderizando Menu...'); };
+export const renderOrderScreen = () => { console.log('OrderController: Renderizando Itens Selecionados...'); };
 export const handleSendSelectedItems = () => { alert('Função de Envio KDS (Marcha) em desenvolvimento.'); };
