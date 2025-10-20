e9b1b3381fc9a277149c5a78e12461bb1bcc3d98
fatormd <agenciafgm@gmail.com>
2025-10-20 13:07:09 -0300

    Create orderController.js

---
+++ controllers/controllers/orderController.js
@@ -0,0 +1,4 @@
+// --- CONTROLLERS/ORDERCONTROLLER.JS ---
+export const renderMenu = () => { console.log('OrderController: Renderizando Menu...'); };
+export const renderOrderScreen = () => { console.log('OrderController: Renderizando Itens Selecionados...'); };
+export const handleSendSelectedItems = () => { alert('Função de Envio KDS (Marcha) em desenvolvimento.'); };
